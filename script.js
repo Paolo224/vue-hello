1ebaba4b7613af489c6818ef76c186f25d530892
Paolo224 <picariellopaolo224@gmail.com>
2022-12-12 16:11:14 +0100

    Bonus immagine tramite Vue

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const { crateApp } = Vue
 .createApp({
     data(){
         return {
-            message : 'Hello Vue3'
+            message : 'Hello Vue3',
+            img : './img/vue.png',
+            imgSize : 'img-fluid'
         }
     }
 }).mount('#app');
